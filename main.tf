e6b36910c895b9bdd5fe6ef2a858e9cf43029847
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-02-22 15:47:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-// To Generate Private Key
+
 resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -24,13 +24,13 @@ variable "key_name" {
   default     = "yura"
 }
 
-// Create Key Pair for Connecting EC2 via SSH
+
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
-// Save PEM file locally
+
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa_4096.private_key_pem
   filename = var.key_name
@@ -40,7 +40,7 @@ resource "local_file" "private_key" {
   }
 }
 
-# Create a security group
+
 resource "aws_security_group" "sg_ec2" {
   name        = "sg_ec2"
   description = "Security group for EC2"
