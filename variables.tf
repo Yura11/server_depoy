93a84856a36ef1d9e697d40daedceaca0e668a85
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-02-22 13:51:31 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+ariable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
