cb89a30a57cbf5c50c0805409ac42d24e1709227
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-03-11 12:47:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,6 +59,13 @@ resource "aws_security_group" "sg_ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
