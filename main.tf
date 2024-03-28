9a064e18e61c642e7aeee6f4e1002f8692d84a36
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-03-28 15:31:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -140,7 +140,7 @@ resource "null_resource" "run_ansible" {
   depends_on = [local_file.dynamic_inventory]
 
   provisioner "local-exec" {
-    command     = "ansible-playbook -i dynamic_inventory.ini docker.yml"
+    command     = "ansible-playbook -i dynamic_inventory.ini serverdp.yml"
     working_dir = path.module
   }
 }
